6b9965f83ccd6193d08a3c3b5db4e8a10b3969ec
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-14 00:28:44 -0800

    commit tree 474b4604aca8557ec9ffa7cd9a6187aae41f025d

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count     #remove attribute accessor for genre & add a attribute reader instead
+  attr_reader :title, :genre
+
+  GENRES = []    #THIS IS A CLASS CONSTANT
 
   def initialize(title)
     @title = title
@@ -10,4 +12,19 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
 end
+
+
+#this syntax, Book::GENRES teslls us about a couple of things.
+#     1. whenever we see :: in indicates "name spacing". in other words, it tells us that the thing on the right side of the colons is a thing defined within the context, or name space, of the thing on the left side of the colons.
+#     2. in this case, this is telling us that there is a constant, GENRES(we know its a constant because it's all written in all caps), that is defined within our Book class
+#     3. this is known as a Class constant
+
+
+#REMEBER:::: OUR genre= method AND OUR genre METHOD IS CREATED BY THE FOLLOWING LINE OF CODE---> attr_accessor for :genre
